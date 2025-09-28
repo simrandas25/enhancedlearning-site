b1e4672770d8a52425bfcc2ad7912f2246c90c3b
Simran Das <simrandas@Simrans-MacBook-Pro-2.local>
2025-09-28 15:37:43 -0400

    fix(blog): align author PageProps for Next 15 (await params)

--- fenago21/next.config.js
+++ fenago21/next.config.js
@@ -1,14 +1,20 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
       // NextJS <Image> component needs to whitelist domains for src={}
-      "lh3.googleusercontent.com",
-      "pbs.twimg.com",
-      "images.unsplash.com",
-      "logos-world.net",
+      'lh3.googleusercontent.com',
+      'pbs.twimg.com',
+      'images.unsplash.com',
+      'logos-world.net',
     ],
+  },
+  experimental: {
+    // Tell Next where the monorepo/project root is so it picks the right lockfile
+    outputFileTracingRoot: path.resolve(__dirname, '..'),
   },
 };
 
